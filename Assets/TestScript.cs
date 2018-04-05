6010024827cf473f318fd160befd44549d68ba9f
Jordan Wong <jordanwongwk@gmail.com>
2018-04-05 15:41:46 +0800

    Creating scene and script

---
+++ Assets/TestScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Testing 1 2 3");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
